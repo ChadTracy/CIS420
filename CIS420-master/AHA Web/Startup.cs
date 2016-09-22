d36a120d8559868e5359ff79eb0cd0d5e8b31011
ChadTracy <chad.tracy95@gmail.com>
2016-09-22 13:59:20 -0400

    initial student and parent page

---
+++ CIS420-master/AHA Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AHA_Web.Startup))]
+namespace AHA_Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
