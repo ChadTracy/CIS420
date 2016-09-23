1ae493aa73e18878c1b11d0d3a6dc7d99824ba52
WABENE01 <WABENE01@louisville.edu>
2016-09-23 13:55:45 -0400

    Added 19 Classes

---
+++ AHA Web/Models/DonorsAttendence.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AHA_Web.Models
+{
+    public partial class DonorsAttendence
+    {
+
+        public string Donor_ID { get; set; }
+
+
+        public string Event_ID { get; set; }
+
+        public TimeSpan? Sign_In_Time { get; set; }
+
+        public TimeSpan? Sign_Out_Time { get; set; }
+
+        public virtual Donor Donor { get; set; }
+
+        public virtual Event Event { get; set; }
+    }
+}
---
+++ AHA Web/Models/Grant.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AHA_Web.Models
+{
+    public partial class Grant
+    {
+
+        public string Grant_ID { get; set; }
+
+
+        public string Grantor_ID { get; set; }
+
+        public string Grant_Name { get; set; }
+
+        public DateTime? Grant_Due_Date { get; set; }
+
+        public decimal? Grant_Amount { get; set; }
+
+
+        public string Grant_Type { get; set; }
+
+
+        public string Grant_status { get; set; }
+
+
+        public string Author { get; set; }
+
+        public virtual Grantor Grantor { get; set; }
+    }
+}
