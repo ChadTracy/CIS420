4d60397172630abadca40a69f7e71fa051341711
WABENE01 <WABENE01@louisville.edu>
2016-10-05 17:16:05 -0400

    Added Controllers, Added Views, Edited Web.Config to point Local instead of live database.

---
+++ AHA Web/Controllers/Attendence/DonorsAttendenceController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AHA_Web.Models;
+
+namespace AHA_Web.Controllers.Attendence
+{
+    public class DonorsAttendenceController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: DonorsAttendence
+        public ActionResult Index()
+        {
+            var donorAttendence = db.DonorAttendence.Include(d => d.Donor);
+            return View(donorAttendence.ToList());
+        }
+
+        // GET: DonorsAttendence/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DonorsAttendence donorsAttendence = db.DonorAttendence.Find(id);
+            if (donorsAttendence == null)
+            {
+                return HttpNotFound();
+            }
+            return View(donorsAttendence);
+        }
+
+        // GET: DonorsAttendence/Create
+        public ActionResult Create()
+        {
+            ViewBag.Donor_ID = new SelectList(db.Donors, "Donor_ID", "First_Name");
+            return View();
+        }
+
+        // POST: DonorsAttendence/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Donor_ID,Event_ID,Sign_In_Time,Sign_Out_Time")] DonorsAttendence donorsAttendence)
+        {
+            if (ModelState.IsValid)
+            {
+                db.DonorAttendence.Add(donorsAttendence);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Donor_ID = new SelectList(db.Donors, "Donor_ID", "First_Name", donorsAttendence.Donor_ID);
+            return View(donorsAttendence);
+        }
+
+        // GET: DonorsAttendence/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DonorsAttendence donorsAttendence = db.DonorAttendence.Find(id);
+            if (donorsAttendence == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Donor_ID = new SelectList(db.Donors, "Donor_ID", "First_Name", donorsAttendence.Donor_ID);
+            return View(donorsAttendence);
+        }
+
+        // POST: DonorsAttendence/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Donor_ID,Event_ID,Sign_In_Time,Sign_Out_Time")] DonorsAttendence donorsAttendence)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(donorsAttendence).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Donor_ID = new SelectList(db.Donors, "Donor_ID", "First_Name", donorsAttendence.Donor_ID);
+            return View(donorsAttendence);
+        }
+
+        // GET: DonorsAttendence/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            DonorsAttendence donorsAttendence = db.DonorAttendence.Find(id);
+            if (donorsAttendence == null)
+            {
+                return HttpNotFound();
+            }
+            return View(donorsAttendence);
+        }
+
+        // POST: DonorsAttendence/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            DonorsAttendence donorsAttendence = db.DonorAttendence.Find(id);
+            db.DonorAttendence.Remove(donorsAttendence);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ AHA Web/Controllers/Attendence/StudentAttendenceController.cs
@@ -0,0 +1,132 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using AHA_Web.Models;
+
+namespace AHA_Web.Controllers.Attendence
+{
+    public class StudentAttendenceController : Controller
+    {
+        private ApplicationDbContext db = new ApplicationDbContext();
+
+        // GET: StudentAttendence
+        public ActionResult Index()
+        {
+            var studentAttendence = db.StudentAttendence.Include(s => s.Student);
+            return View(studentAttendence.ToList());
+        }
+
+        // GET: StudentAttendence/Details/5
+        public ActionResult Details(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            StudentAttendence studentAttendence = db.StudentAttendence.Find(id);
+            if (studentAttendence == null)
+            {
+                return HttpNotFound();
+            }
+            return View(studentAttendence);
+        }
+
+        // GET: StudentAttendence/Create
+        public ActionResult Create()
+        {
+            ViewBag.Student_ID = new SelectList(db.Students, "Student_ID", "First_Name");
+            return View();
+        }
+
+        // POST: StudentAttendence/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "Student_ID,Event_ID,Sign_in_Time,Sign_Out_Time")] StudentAttendence studentAttendence)
+        {
+            if (ModelState.IsValid)
+            {
+                db.StudentAttendence.Add(studentAttendence);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.Student_ID = new SelectList(db.Students, "Student_ID", "First_Name", studentAttendence.Student_ID);
+            return View(studentAttendence);
+        }
+
+        // GET: StudentAttendence/Edit/5
+        public ActionResult Edit(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            StudentAttendence studentAttendence = db.StudentAttendence.Find(id);
+            if (studentAttendence == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.Student_ID = new SelectList(db.Students, "Student_ID", "First_Name", studentAttendence.Student_ID);
+            return View(studentAttendence);
+        }
+
+        // POST: StudentAttendence/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Student_ID,Event_ID,Sign_in_Time,Sign_Out_Time")] StudentAttendence studentAttendence)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(studentAttendence).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.Student_ID = new SelectList(db.Students, "Student_ID", "First_Name", studentAttendence.Student_ID);
+            return View(studentAttendence);
+        }
+
+        // GET: StudentAttendence/Delete/5
+        public ActionResult Delete(string id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            StudentAttendence studentAttendence = db.StudentAttendence.Find(id);
+            if (studentAttendence == null)
+            {
+                return HttpNotFound();
+            }
+            return View(studentAttendence);
+        }
+
+        // POST: StudentAttendence/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(string id)
+        {
+            StudentAttendence studentAttendence = db.StudentAttendence.Find(id);
+            db.StudentAttendence.Remove(studentAttendence);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
