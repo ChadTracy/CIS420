6282fcf45653a46853beb1fcf8e9756c70895391
Nathan Stopinski <ntstop01@louisville.edu>
2016-10-10 10:41:29 -0400

    removed calendar

--- AHA Web/Controllers/HomeController.cs
+++ AHA Web/Controllers/HomeController.cs
@@ -17,8 +17,6 @@ namespace AHA_Web.Controllers
 {
     public class HomeController : Controller
     {
-        private Event db = new Event();
-
         public ActionResult Index()
         {
             return View();
@@ -26,15 +24,11 @@ namespace AHA_Web.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
-
             return View();
         }
 
         public ActionResult Contact()
         {
-            ViewBag.Message = "Your contact page.";
-
             return View();
         }
 
@@ -77,73 +71,5 @@ namespace AHA_Web.Controllers
         public ActionResult Ambassadors() { return View(); }
         public ActionResult TJXScholarship() { return View(); }
         public ActionResult BAM() { return View(); }
-
-
-        private readonly DbContext _db = new ApplicationDbContext();
-
-        public ActionResult Calendar()
-        {
-            var scheduler = new DHXScheduler(this);
-            scheduler.Skin = DHXScheduler.Skins.Flat;
-
-            scheduler.Config.first_hour = 6;
-            scheduler.Config.last_hour = 20;
-
-            scheduler.LoadData = true;
-            scheduler.EnableDataprocessor = true;
-
-            return View(scheduler);
-        }
-
-        public ContentResult Data(DateTime from, DateTime to)
-        {
-            var apps = _db.Set<Event>().Where(e => e.Event_Start_Date < to && e.Event_End_Date >= from).ToList();
-            return new SchedulerAjaxData(apps);
-        }
-
-        public ContentResult Data()
-        {
-            var apps = _db.Set<Event>().ToList();
-            return new SchedulerAjaxData(apps);
-        }
-
-        public ActionResult Save(int? id, FormCollection actionValues)
-        {
-            var action = new DataAction(actionValues);
-
-            try
-            {
-                var changedEvent = DHXEventsHelper.Bind<Event>(actionValues);
-                switch (action.Type)
-                {
-                    case DataActionTypes.Insert:
-                        Event EV = new Event();
-                        EV.Event_Start_Date = changedEvent.Event_Start_Date;
-                        EV.Event_End_Date = changedEvent.Event_End_Date;
-                        EV.Description = changedEvent.Description;
-                        _db.Set<Event>().Add(EV);
-                        _db.SaveChanges();
-                        
-                        break;
-                }
-                _db.SaveChanges();
-                action.TargetId = Convert.ToInt64(changedEvent.Event_ID);
-            }
-            catch (Exception)
-            {
-                action.Type = DataActionTypes.Error;
-            }
-
-            return (new AjaxSaveResponse(action));
-        }
-
-        protected override void Dispose(bool disposing)
-        {
-            if (disposing)
-            {
-                _db.Dispose();
-            }
-            base.Dispose(disposing);
-        }
     }
 }
--- AHA Web/Models/Event.cs
+++ AHA Web/Models/Event.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-
 using DHTMLX.Scheduler;
 using DHTMLX.Scheduler.Data;
 using DHTMLX.Common;
@@ -13,60 +9,13 @@ namespace AHA_Web.Models
 {
     public partial class Event
     {
-
-        public Event()
-        {
-            DonorsAttendences = new HashSet<DonorsAttendence>();
-            VolunteerHistories = new HashSet<VolunteerHistory>();
-            StudentAttendences = new HashSet<StudentAttendence>();
-        }
-
         [Required]
-        [Key]
-        [Display(Name = "Event ID")]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int Event_ID { get; set; }
+        public int EventID { get; set; }
 
-        [Required]
-        [MaxLength(100)]
-        [Display(Name = "Description")]
-        public string Description { get; set; }
+        public string text { get; set; }
 
-        [Required]
-        [MaxLength(50)]
-        [Display(Name = "Event Title")]
-        public string Event_Name { get; set; }
+        public DateTime start_date { get; set; }
 
-        [Required]
-        [Display(Name = "Attendence Cost")]
-        [DataType(DataType.Currency, ErrorMessage = "Please Provide a Currency Value")]
-        public decimal? Attendence_Cost { get; set; }
-
-        [Required]
-        [Display(Name = "Type of Event")]
-        [MaxLength(20)] 
-        public string Event_Type { get; set; }
-
-       
-        [Display(Name = "Starting Date")]
-        [DataType(DataType.Date, ErrorMessage ="Please Enter a Valid Date")]
-        public DateTime? Event_Start_Date { get; set; }
-
-        [Display(Name = "Ending Date")]
-        [DataType(DataType.Date, ErrorMessage = "Please Enter a Valid Date")]
-        public DateTime? Event_End_Date { get; set; }
-
-        [Required]
-        [DataType(DataType.Time,ErrorMessage = "Please Enter a Valid Time")]
-        public TimeSpan? Event_Time { get; set; }
-
-
-        public virtual ICollection<DonorsAttendence> DonorsAttendences { get; set; }
-
-
-        public virtual ICollection<VolunteerHistory> VolunteerHistories { get; set; }
-
-
-        public virtual ICollection<StudentAttendence> StudentAttendences { get; set; }
+        public DateTime end_date { get; set; }
     }
 }
