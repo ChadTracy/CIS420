ba3f4a302f6f01554b1b828e90d960ea665b63ca
jamesericryg <jamesericryg@gmail.com>
2016-10-16 15:33:51 -0400

    Switched our problematic dropdown to a textbox

--- AHA Web/Controllers/AccountController.cs
+++ AHA Web/Controllers/AccountController.cs
@@ -191,7 +191,6 @@ namespace AHA_Web.Controllers
         [AllowAnonymous]
         public ActionResult Register()
         {
-            ViewBag.AccountType = new SelectList(new[] { "Admin", "Staff", "BoardMember","Donor", "Volunteer", "Student", "Parent", "Quarantine" });
             return View();
         }
 
