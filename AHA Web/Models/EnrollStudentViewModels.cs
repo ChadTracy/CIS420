4012312abc723d330609f76cd92484439bedc426
jamesericryg <jamesericryg@gmail.com>
2016-11-04 14:59:50 -0400

    Program Enrollment working, still needs what I posted in slack.

---
+++ AHA Web/Models/EnrollStudentViewModels.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AHA_Web.Models
+{
+    public class EnrollStudentViewModel
+    {
+        public UsersViewModel UserModel { get; set; }
+        public string Program_ID { get; set; }
+    }
+}
